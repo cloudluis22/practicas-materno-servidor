df53fedc0a49fefa0ede38080ac272f548e62478
cloudluis22 <cloudluis22@hotmail.com>
2022-04-15 20:58:47 -0500

    Finalizado tabla alimentacionBebe

--- routes/alimentacionBebe.js
+++ routes/alimentacionBebe.js
@@ -30,35 +30,47 @@ router
   .post((req, res) => {
     const IdPaciente = req.params.id;
     const TomaPechoEdad = req.body.TomaPechoEdad;
+    const LblTomaPechoEdad = req.body.LblTomaPechoEdad;
     const FrecuenciaAlimentacionPecho = req.body.FrecuenciaAlimentacionPecho;
+    const LblFrecuenciaAlimentacionPecho =
+      req.body.LblFrecuenciaAlimentacionPecho;
     const TipoDeAlimentacion = req.body.TipoDeAlimentacion;
     const UsabaBiberon = req.body.UsabaBiberon;
+    const LblUsabaBiberon = req.body.LblUsabaBiberon;
     const ContenidoBiberon = req.body.ContenidoBiberon;
     const EdadYaNoTomaBiberon = req.body.EdadYaNoTomaBiberon;
+    const UsabaChupon = req.body.UsabaChupon;
     const LblUsabaChupon = req.body.LblUsabaChupon;
     const ContenidoChupon = req.body.ContenidoChupon;
     const EdadYaNoUsaChupon = req.body.EdadYaNoUsaChupon;
+    const AlimentacionNocturna = req.body.AlimentacionNocturna;
     const LblAlimentacionNocturna = req.body.LblAlimentacionNocturna;
     const LimpiaSuBoquita = req.body.LimpiaSuBoquita;
+    const BebeConsumeSolidos = req.body.BebeConsumeSolidos;
     const LblBebeConsumeSolidos = req.body.LblBebeConsumeSolidos;
 
     const peticionSql =
-      'INSERT INTO alimentacionbebe (IdPaciente, TomaPechoEdad, FrecuenciaAlimentacionPecho, TipoDeAlimentacion,  UsabaBiberon, ContenidoBiberon, EdadYaNoTomaBiberon, UsabaChupon, ContenidoChupon, EdadYaNoUsaChupon,  AlimentacionNocturna, LimpiaSuBoquita, BebeConsumeSolidos,) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+      'INSERT INTO alimentacionbebe (IdPaciente, TomaPechoEdad, LblTomaPechoEdad, LblFrecuenciaAlimentacionPecho, TipoDeAlimentacion, UsabaBiberon, LblUsabaBiberon, ContenidoBiberon, EdadYaNoTomaBiberon, UsabaChupon, LblUsabaChupon, ContenidoChupon, EdadYaNoUsaChupon, AlimentacionNocturna, LblAlimentacionNocturna, LimpiaSuBoquita, BebeConsumeSolidos, LblBebeConsumeSolidos) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
     db.query(
       peticionSql,
       [
         IdPaciente,
         TomaPechoEdad,
-        FrecuenciaAlimentacionPecho,
+        LblTomaPechoEdad,
+        LblFrecuenciaAlimentacionPecho,
         TipoDeAlimentacion,
         UsabaBiberon,
+        LblUsabaBiberon,
         ContenidoBiberon,
         EdadYaNoTomaBiberon,
+        UsabaChupon,
         LblUsabaChupon,
         ContenidoChupon,
         EdadYaNoUsaChupon,
+        AlimentacionNocturna,
         LblAlimentacionNocturna,
         LimpiaSuBoquita,
+        BebeConsumeSolidos,
         LblBebeConsumeSolidos,
       ],
       (err, result) => {
@@ -74,34 +86,46 @@ router
   .put((req, res) => {
     const IdPaciente = req.params.id;
     const TomaPechoEdad = req.body.TomaPechoEdad;
+    const LblTomaPechoEdad = req.body.LblTomaPechoEdad;
     const FrecuenciaAlimentacionPecho = req.body.FrecuenciaAlimentacionPecho;
+    const LblFrecuenciaAlimentacionPecho =
+      req.body.LblFrecuenciaAlimentacionPecho;
     const TipoDeAlimentacion = req.body.TipoDeAlimentacion;
     const UsabaBiberon = req.body.UsabaBiberon;
+    const LblUsabaBiberon = req.body.LblUsabaBiberon;
     const ContenidoBiberon = req.body.ContenidoBiberon;
     const EdadYaNoTomaBiberon = req.body.EdadYaNoTomaBiberon;
+    const UsabaChupon = req.body.UsabaChupon;
     const LblUsabaChupon = req.body.LblUsabaChupon;
     const ContenidoChupon = req.body.ContenidoChupon;
     const EdadYaNoUsaChupon = req.body.EdadYaNoUsaChupon;
+    const AlimentacionNocturna = req.body.AlimentacionNocturna;
     const LblAlimentacionNocturna = req.body.LblAlimentacionNocturna;
     const LimpiaSuBoquita = req.body.LimpiaSuBoquita;
+    const BebeConsumeSolidos = req.body.BebeConsumeSolidos;
     const LblBebeConsumeSolidos = req.body.LblBebeConsumeSolidos;
 
     const peticionSql =
-      'UPDATE alimentacionbebe SET IdPaciente = ?, TomaPechoEdad = ?, FrecuenciaAlimentacionPecho = ? , TipoDeAlimentacion = ?, UsabaBiberon = ?, ContenidoBiberon = ?, EdadYaNoTomaBiberon = ?, UsabaChupon = ?, ContenidoChupon = ?, EdadYaNoUsaChupon = ?, AlimentacionNocturna = ?, LimpiaSuBoquita = ?, BebeConsumeSolidos = ? WHERE IdPaciente = ?';
+      'UPDATE alimentacionbebe SET TomaPechoEdad = ?, LblTomaPechoEdad = ?, LblFrecuenciaAlimentacionPecho = ?, TipoDeAlimentacion = ?, UsabaBiberon = ?, LblUsabaBiberon = ?, ContenidoBiberon = ?, EdadYaNoTomaBiberon = ?, UsabaChupon = ?, LblUsabaChupon = ?, ContenidoChupon = ?, EdadYaNoUsaChupon = ?, AlimentacionNocturna = ?, LblAlimentacionNocturna = ?, LimpiaSuBoquita = ?, BebeConsumeSolidos = ?, LblBebeConsumeSolidos = ? WHERE IdPaciente = ?';
     db.query(
       peticionSql,
       [
         TomaPechoEdad,
-        FrecuenciaAlimentacionPecho,
+        LblTomaPechoEdad,
+        LblFrecuenciaAlimentacionPecho,
         TipoDeAlimentacion,
         UsabaBiberon,
+        LblUsabaBiberon,
         ContenidoBiberon,
         EdadYaNoTomaBiberon,
+        UsabaChupon,
         LblUsabaChupon,
         ContenidoChupon,
         EdadYaNoUsaChupon,
+        AlimentacionNocturna,
         LblAlimentacionNocturna,
         LimpiaSuBoquita,
+        BebeConsumeSolidos,
         LblBebeConsumeSolidos,
         IdPaciente,
       ],
